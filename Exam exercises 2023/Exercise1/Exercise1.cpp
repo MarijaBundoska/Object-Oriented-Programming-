cd9b25fd44391cdd70de2a23006b207566c89a8e
MarijaBundoska <b.marijaa2@gmail.com>
2026-09-09 06:36:17 +0200

    Update Exercise1.cpp

--- Exam exercises 2023/Exercise1/Exercise1.cpp
+++ Exam exercises 2023/Exercise1/Exercise1.cpp
@@ -45,7 +45,7 @@ private:
         strcpy(this->url, other.url);
     }
 public:
-    OnlineBook(char *isbn, char *title, char *author, double price, char *url, int size=0):Book(isbn, title, author, price){
+    OnlineBook(char *isbn, char *title, char *author, double price=0, char *url, int size=0):Book(isbn, title, author, price){
         this->size = size;
         this->url = new char[strlen(url)+1];
         strcpy(this->url, url);
